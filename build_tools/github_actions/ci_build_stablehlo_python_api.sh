293157c0ae2f8d536a4db34560273cf43fd8e92b
Kevin Gleason <gleasonk@google.com>
2022-09-16 20:55:47 +0000

    revert python build file

--- build_tools/github_actions/ci_build_stablehlo_python_api.sh
+++ build_tools/github_actions/ci_build_stablehlo_python_api.sh
@@ -12,18 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [[ $# -ne 2 ]] ; then
-  echo "Usage: $0 <llvm_project_dir> <stablehlo_api_build_dir>>"
+if [[ $# -ne 3 ]] ; then
+  echo "Usage: $0 <llvm_project_dir> <stablehlo_api_build_dir> <stablehlo_root_dir>"
   exit 1
 fi
 
 LLVM_PROJECT_DIR="$1"
 STABLEHLO_PYTHON_BUILD_DIR="$2"
+STABLEHLO_ROOT_DIR="$3"
 
 # Configure StableHLO Python Bindings
-BUILD_TOOLS_DIR="$(dirname "$(readlink -f "$0")")"
-$BUILD_TOOLS_DIR/ci_build_llvm.sh "$1" "$2"
-
+cmake -GNinja \
+  -B"$STABLEHLO_PYTHON_BUILD_DIR" \
+  $LLVM_PROJECT_DIR/llvm \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_ENABLE_PROJECTS=mlir \
+  -DLLVM_EXTERNAL_PROJECTS=stablehlo \
+  -DLLVM_EXTERNAL_STABLEHLO_SOURCE_DIR="$STABLEHLO_ROOT_DIR" \
+  -DLLVM_TARGETS_TO_BUILD=host \
+  -DPython3_EXECUTABLE=$(which python3) \
+  -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
+  -DSTABLEHLO_ENABLE_BINDINGS_PYTHON=ON \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_C_COMPILER_LAUNCHER=ccache
 
 # Build and Check StableHLO Python Bindings
 cd "$STABLEHLO_PYTHON_BUILD_DIR"
